e095a7334376c647e5dab3d2e6b7332c7401ae4f
Chen Fang <chenfanggm@hotmail.com>
2026-01-19 13:51:31 +0800

    [auth-server]

--- netlogic.core/Command/Buffer/EngineCommandBatch.cs
+++ netlogic.core/Command/Buffer/EngineCommandBatch.cs
@@ -1,5 +1,8 @@
 namespace Sim
 {
+    /// <summary>
+    /// Bundles a scheduled tick, client command sequence, and command list for processing.
+    /// </summary>
     public readonly struct EngineCommandBatch(int scheduledTick, uint clientCmdSeq, List<EngineCommand> commands)
     {
         public readonly int ScheduledTick = scheduledTick;
